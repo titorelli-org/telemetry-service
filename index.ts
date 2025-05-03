53b8fab81866a9147c8520eef6e28ef3b834df06
ychebotaev <yury.79120345101@gmail.com>
2025-05-03 15:44:31 +0500

    fix: new param to UpdateRepo

--- index.ts
+++ index.ts
@@ -2,6 +2,13 @@ import fastify from "fastify";
 import { UpdateRepo, Wal, env, logger } from "./lib";
 import { MongoClient } from "mongodb";
 import { ChatRepo } from "./lib/repositories/ChatRepo";
+import { MembersRepo } from "./lib/repositories/MembersRepo";
+import type {
+  ChatFullInfo,
+  ChatMember,
+  Update,
+  UserFromGetMe,
+} from "@grammyjs/types";
 
 export * from "./lib";
 
@@ -11,16 +18,21 @@ export const createServer = async () => {
     new Wal("titorelli", "updates", logger),
     mongoClient.db("titorelli").collection("updates"),
     new ChatRepo(mongoClient.db("titorelli").collection("chats"), logger),
+    new MembersRepo(mongoClient.db("titorelli").collection("members"), logger),
     logger,
   );
   const server = fastify({ loggerInstance: logger, trustProxy: true });
 
-  server.post<{ Body: Record<string, unknown> }>(
-    "/update",
-    async ({ body }) => {
-      await updateRepo.insert(body);
-    },
-  );
+  server.post<{
+    Body: {
+      update: Update;
+      chat: ChatFullInfo;
+      author: ChatMember;
+      me: UserFromGetMe;
+    };
+  }>("/update", async ({ body }) => {
+    await updateRepo.insert(body);
+  });
 
   return server;
 };
